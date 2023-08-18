3891537023a7ccf5a0b90ca542fd9fcb4d281882
Riyad <Riyad9876@gmail.com>
2023-08-18 11:06:02 -0400

    weather api, .env

--- PortfolioReact/src/pages/Weather.jsx
+++ PortfolioReact/src/pages/Weather.jsx
@@ -1,9 +1,20 @@
-
+const key = import.meta.env.VITE_WEATHER_KEY
 
 export default function Weather(){
 
 
 
+
+
+    console.log(key)
+
+    const getWeather= async() =>{
+        const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${40.71}&lon=${74}&appid=${key}`)
+        const data = await res.json()
+        console.log(data)
+    }
+
+    getWeather()
 
     return (
         <>

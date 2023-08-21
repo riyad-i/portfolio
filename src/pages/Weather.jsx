460a07b91d1f0ab5150af9dfa5cf7d254cd651ec
Riyad <Riyad9876@gmail.com>
2023-08-21 12:08:57 -0400

    added overcast

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -50,7 +50,7 @@ export default function Weather(){
         </form>
 
 
-        {weather.current ? <h3> Currently {weather.current.weather_descriptions[0]} and {conversion(weather.current.temperature)} Degrees Fahrenheit in {weather.location.name}</h3> : <h3>Loading Weather</h3>}
+        {weather.current ? <h3> Currently {weather.current.weather_descriptions[0]} and {conversion(weather.current.temperature)} Degrees Fahrenheit in {weather.location.name}</h3> : <h3>Search for a city's weather</h3>}
 
         
         <br/>
@@ -63,6 +63,7 @@ export default function Weather(){
         { weather.current && weather.current.weather_descriptions[0] == 'Partly cloudy' ? <img src="https://images.unsplash.com/photo-1597621864521-93dfdb10c6b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFydGx5JTIwY2xvdWR5fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" height={300}/> : null }
         { weather.current && weather.current.weather_descriptions[0] == 'Clear' ? <img src="https://images.unsplash.com/photo-1616249807402-9dae436108cf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2xlYXIlMjB3ZWF0aGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" height={300}/> : null }
         { weather.current && weather.current.weather_descriptions[0] == 'Sunny' ? <img src="https://images.unsplash.com/photo-1598965914211-6ec6872593a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fHN1bnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" height={300}/> : null }
+        { weather.current && weather.current.weather_descriptions[0] == 'Overcast' ? <img src="https://images.unsplash.com/photo-1499956827185-0d63ee78a910?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8b3ZlcmNhc3R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" height={300}/> : null }
         </>
     )
 

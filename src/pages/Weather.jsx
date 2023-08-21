0b3a96c7453ee34b0b8c3f13751319594de39155
Riyad <Riyad9876@gmail.com>
2023-08-21 02:34:50 -0400

    displaying weather info

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,19 +1,23 @@
 const key = import.meta.env.VITE_WEATHER_STACK_KEY
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 export default function Weather(){
 
-
+    const [weather , setWeather] = useState({})
 
     //https://api.openweathermap.org/data/3.0/onecall?lat=${40}&lon=${74}&appid=${key}
 
-    console.log(key)
+
+    const conversion = (celsius) => {
+        return (celsius * 9/5) + 32
+    }
 
     const getWeather= async() =>{
         const res = await fetch(`http://api.weatherstack.com/current?access_key=${key}&query=New%20York`)
         const data = await res.json()
-        console.log(data)
+        console.log(data);
+        setWeather(data)
     }
 
     useEffect(()=> {getWeather()},[])
@@ -21,7 +25,11 @@ export default function Weather(){
     return (
         <>
         <h1>Weather App</h1>
+        {weather.current ? <h3> Currently {weather.current.weather_descriptions[0]} and {conversion(weather.current.temperature)} Degrees Fahrenheit in New York</h3> : <h3>Loading Weather</h3>}
+
         
+        <br/>
+        {weather.current ? <img src={weather.current.weather_icons[0]}/>: null }
         </>
     )
 

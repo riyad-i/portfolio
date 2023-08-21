1204cf0ae2a23b0df306a0b35018e78795f6c3fe
Riyad <Riyad9876@gmail.com>
2023-08-21 02:16:12 -0400

    weather stack api functional

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,20 +1,22 @@
-const key = import.meta.env.VITE_WEATHER_KEY
+const key = import.meta.env.VITE_WEATHER_STACK_KEY
+import { useEffect } from "react"
+
 
 export default function Weather(){
 
 
 
-
+    //https://api.openweathermap.org/data/3.0/onecall?lat=${40}&lon=${74}&appid=${key}
 
     console.log(key)
 
     const getWeather= async() =>{
-        const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${40.71}&lon=${74}&appid=${key}`)
+        const res = await fetch(`http://api.weatherstack.com/current?access_key=${key}&query=New%20York`)
         const data = await res.json()
         console.log(data)
     }
 
-    getWeather()
+    useEffect(()=> {getWeather()},[])
 
     return (
         <>

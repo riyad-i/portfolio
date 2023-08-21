1a67dcd41f47dad26fb7bbb54b98a09b48e56ae8
Riyad <Riyad9876@gmail.com>
2023-08-21 11:26:46 -0400

    resume to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,9 @@ export default function Footer(){
 
     return (
         <div className="Footer">
-            
+
             <a href="https://www.linkedin.com/in/riyad-i">LinkedIn</a>
+            <a className="button" href="../../Resumev4.pdf" download = "Resumev4.pdf">Resume</a>
             <a href="https://github.com/riyad-i">Github</a>
             
         </div>

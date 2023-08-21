0c91996dfb4d25e4036e17f2912f227dd1023bae
Riyad <Riyad9876@gmail.com>
2023-08-21 10:22:28 -0400

    update readme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ export default function Footer(){
 
     return (
         <div className="Footer">
-            <a href="www.linkedin.com/in/riyad-i">LinkedIn</a>
+            
+            <a href="https://www.linkedin.com/in/riyad-i">LinkedIn</a>
             <a href="https://github.com/riyad-i">Github</a>
             
         </div>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,38 @@
+// import React from 'react'
+// import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+// import ReactPDF from '@react-pdf/renderer';
 
+// import resume from '../../Resumev4.pdf'
+
+// import { Document, Page, pdfjs } from 'react-pdf'
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdn.js.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
+
+// const styles = StyleSheet.create({
+//     page: {
+//       flexDirection: 'row',
+//       backgroundColor: '#E4E4E4'
+//     },
+//     section: {
+//       margin: 10,
+//       padding: 10,
+//       flexGrow: 1
+//     }
+//   });
+  
+//   // Create Document Component
+//   const MyDocument = () => (
+//     <Document>
+//       <Page size="A4" style={styles.page}>
+//         <View style={styles.section}>
+//           <Text>Section #1</Text>
+//         </View>
+//         <View style={styles.section}>
+//           <Text>Section #2</Text>
+//         </View>
+//       </Page>
+//     </Document>
+//   );
 
 export default function Main(){
 
@@ -8,7 +42,12 @@ export default function Main(){
     return (
         <>
         <h1>Portfolio</h1>
-        
+        {/* <Document file={resume} onLoadError={console.error}  >
+            <Page pageIndex={0}/>
+
+        </Document> */}
+        {/* ReactPDF.render(<MyDocument />, `../../Resumev4.pdf`); */}
+        {/* <MyDocument></MyDocument> */}
         </>
     )
 
